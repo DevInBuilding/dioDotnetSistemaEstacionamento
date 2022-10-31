48907864e5edaf6fc2807ed52b7b850078aab900
Lucas Coelho Barros <lucasbarrosprofessional@gmail.com>
2022-10-31 17:11:22 -0300

    Creating new dotnet console application

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using Projeto_Sistema_Estacionamento.Models;
+
+Estacionamento e = new Estacionamento(10.0M, 5.00M);
+e.AdicionarVeiculo();
